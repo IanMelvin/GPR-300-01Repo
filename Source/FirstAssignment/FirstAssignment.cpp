9d43d8a9abd4d8725484f33e780dbc5f44164dad
Ian Melvin <ian.melvin@mymail.champlain.edu>
2022-02-01 18:57:50 -0500

    Pop quiz with recording

---
+++ Source/FirstAssignment/FirstAssignment.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstAssignment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstAssignment, "FirstAssignment" );
---
+++ Source/FirstAssignment/FirstAssignmentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstAssignmentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTASSIGNMENT_API AFirstAssignmentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
